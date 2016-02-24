a322fa797a53c4e6171f75e11e2aeaba389e12e9
Nathan Dhami <nathan_dhami@sfu.ca>
2016-02-23 20:32:17 -0800

    all tokens have been defined in both .y and .lex files

---
+++ hw2/answer/decaf-ast.cc
@@ -0,0 +1,184 @@
+
+#include "usingcpp-defs.h"
+#include <list>
+#include <ostream>
+#include <iostream>
+#include <sstream>
+
+#ifndef YYTOKENTYPE
+#include "decaf-ast.tab.h"
+#endif
+
+using namespace std;
+
+string BinaryOpString(int Op) {
+	switch (Op) {
+		case T_PLUS: return string("Plus");
+  		case T_MINUS: return string("Minus");
+  		case T_MULT: return string("Mult");
+  		case T_DIV: return string("Div");
+  		case T_MOD: return string("Mod");
+		default: throw runtime_error("unknown type in BinaryOpString call");
+	}
+}
+
+string UnaryOpString(int Op) {
+	switch (Op) {
+  		case T_MINUS: return string("UnaryMinus");
+  		case T_NOT: return string("Not");
+		default: throw runtime_error("unknown type in UnaryOpString call");
+	}
+}
+
+string convertInt(int number) {
+	stringstream ss;
+	ss << number;
+	return ss.str();
+}
+
+/// decafAST - Base class for all abstract syntax tree nodes.
+class decafAST {
+public:
+  virtual ~decafAST() {}
+  virtual string str() { return string(""); }
+};
+
+string getString(decafAST *d) {
+	if (d != NULL) {
+		return d->str();
+	} else {
+		return string("None");
+	}
+}
+
+string buildString1(const char *Name, decafAST *a) {
+	return string(Name) + "(" + getString(a) + ")";
+}
+
+string buildString1(const char *Name, string a) {
+	return string(Name) + "(" + a + ")";
+}
+
+string buildString2(const char *Name, decafAST *a, decafAST *b) {
+	return string(Name) + "(" + getString(a) + "," + getString(b) + ")";
+}
+
+string buildString2(const char *Name, string a, decafAST *b) {
+	return string(Name) + "(" + a + "," + getString(b) + ")";
+}
+
+string buildString3(const char *Name, decafAST *a, decafAST *b, decafAST *c) {
+	return string(Name) + "(" + getString(a) + "," + getString(b) + "," + getString(c) + ")";
+}
+
+string buildString3(const char *Name, string a, decafAST *b, decafAST *c) {
+	return string(Name) + "(" + a + "," + getString(b) + "," + getString(c) + ")";
+}
+
+
+template <class T>
+string commaList(list<T> vec) {
+	string s("");
+	for (typename list<T>::iterator i = vec.begin(); i != vec.end(); i++) { 
+		s = s + (s.empty() ? string("") : string(",")) + (*i)->str(); 
+	}
+	if (s.empty()) {
+		s = string("None");
+	} 
+	return s;
+}
+
+/// decafStmtList - List of Decaf statements
+class decafStmtList : public decafAST {
+	list<decafAST *> stmts;
+public:
+	decafStmtList() {}
+	~decafStmtList() {
+		for (list<decafAST *>::iterator i = stmts.begin(); i != stmts.end(); i++) { 
+			delete *i;
+		}
+	}
+	int size() { return stmts.size(); }
+	void push_front(decafAST *e) { stmts.push_front(e); }
+	void push_back(decafAST *e) { stmts.push_back(e); }
+	string str() { return commaList<class decafAST *>(stmts); }
+};
+
+/// NumberExprAST - Expression class for integer numeric literals like "12".
+class NumberExprAST : public decafAST {
+	int Val;
+public:
+	NumberExprAST(int val) : Val(val) {}
+	string str() { return buildString1("Number", convertInt(Val)); }
+};
+
+/// StringConstAST - string constant
+class StringConstAST : public decafAST {
+	string StringConst;
+public:
+	StringConstAST(string s) : StringConst(s) {}
+	string str() { return buildString1("StringConstant", "\"" + StringConst + "\""); }
+};
+
+/// BoolExprAST - Expression class for boolean literals: "true" and "false".
+class BoolExprAST : public decafAST {
+	bool Val;
+public:
+	BoolExprAST(bool val) : Val(val) {}
+	string str() { return buildString1("BoolExpr", Val ? string("True") : string("False")); }
+};
+
+/// VariableExprAST - Expression class for variables like "a".
+class VariableExprAST : public decafAST {
+	string Name;
+public:
+	VariableExprAST(string name) : Name(name) {}
+	string str() { return buildString1("VariableExpr", Name); }
+	//const std::string &getName() const { return Name; }
+};
+
+
+/// BinaryExprAST - Expression class for a binary operator.
+class BinaryExprAST : public decafAST {
+	int Op; // use the token value of the operator
+	decafAST *LHS, *RHS;
+public:
+	BinaryExprAST(int op, decafAST *lhs, decafAST *rhs) : Op(op), LHS(lhs), RHS(rhs) {}
+	~BinaryExprAST() { delete LHS; delete RHS; }
+	string str() { return buildString3("BinaryExpr", BinaryOpString(Op), LHS, RHS); }
+};
+
+/// UnaryExprAST - Expression class for a unary operator.
+class UnaryExprAST : public decafAST {
+	int Op; // use the token value of the operator
+	decafAST *Expr;
+public:
+	UnaryExprAST(int op, decafAST *expr) : Op(op), Expr(expr) {}
+	~UnaryExprAST() { delete Expr; }
+	string str() { return buildString2("UnaryExpr", UnaryOpString(Op), Expr); }
+};
+
+/// AssignVarAST - assign value to a variable
+class AssignVarAST : public decafAST {
+	string Name; // location to assign value
+	decafAST *Value;
+public:
+	AssignVarAST(string name, decafAST *value) : Name(name), Value(value) {}
+	~AssignVarAST() { 
+		if (Value != NULL) { delete Value; }
+	}
+	string str() { return buildString2("AssignVar", Name, Value); }
+};
+
+
+/// ProgramAST - the simplified decaf program
+class ProgramAST : public decafAST {
+	decafStmtList *ExternList;
+public:
+	ProgramAST(decafStmtList *externs) : ExternList(externs) {}
+	~ProgramAST() { 
+		if (ExternList != NULL) { delete ExternList; } 
+	}
+	string str() { return buildString1("Program", ExternList); }
+};
+
--- hw2/answer/decaf-ast.y
+++ hw2/answer/decaf-ast.y
@@ -1,25 +1,45 @@
 %{
-#include <stdio.h>
+#include <iostream>
+#include <ostream>
+#include <string>
+#include <cstdlib>
+#include "usingcpp-defs.h"
+
+int yylex(void);
+int yyerror(char *); 
+
+using namespace std;
+
+// print AST?
+bool printAST = true;
+
+#include "decaf-ast.cc"
+
 %}
 
 %union{
-	int number;
-	char* stringVal;
-}
+    class decafAST *ast;
+    std::string *sval;
+    int number;
+    int decaftype;
+ }
+
 
 %token T_AND T_ASSIGN T_BOOL T_BREAK 
-%token T_ CLASS T_COMMA T_COMMENT T_CONTINUE T_DIV 
+%token T_CLASS T_COMMA T_COMMENT T_CONTINUE T_DIV 
 %token T_DOT T_ELSE T_EQ T_EXTENDS T_EXTERN 
-%token T_FOR T_GEQ T_GT T_ID T_IF T_INTTYPE 
+%token T_FOR T_GEQ T_GT T_IF T_INTTYPE 
 %token T_LCB T_LEFTSHIFT T_LEQ T_LPAREN T_LSB T_LT T_MINUS 
 %token T_MOD T_MULT T_NEQ T_NEW T_NOT T_NULL T_OR T_PLUS 
 %token T_RCB T_RETURN T_RIGHTSHIFT T_RPAREN T_RSB T_SEMICOLON 
-%token T_STRINGCONSTANT T_STRINGTYPE T_VOID T_WHILE T_WHITESPACE
+%token T_STRINGTYPE T_VOID T_WHILE T_WHITESPACE
 
-%token <number>    T_TRUE T_FALSE T_INTCONSTANT T_CHARCONSTANT
-%token <stringVal> T_ID T_STRINGCONSTANT
+%token <number> T_CHARCONSTANT T_INTCONSTANT T_FALSE T_TRUE
+%token <sval> T_ID T_STRINGCONSTANT
 
-// Operators and Precedence (high to low)
+%type <ast> rvalue expr constant bool_constant assign
+%type <ast> statement statement_list
+
 %right UMINUS
 %left T_NOT
 %left T_MULT T_DIV T_MOD T_LEFTSHIFT T_RIGHTSHIFT
@@ -28,11 +48,76 @@
 %left T_AND
 %left T_OR
 
+%%
 
+start: program
+
+program: statement_list
+    {
+        ProgramAST *prog = new ProgramAST((decafStmtList *)$1);
+                if (printAST) {
+                        cout << getString(prog) << endl;
+                }
+        delete prog;
+    }
+
+
+statement_list: statement statement_list
+    { decafStmtList *slist = (decafStmtList *)$2; slist->push_front($1); $$ = slist; }
+    | /* empty */ 
+    { decafStmtList *slist = new decafStmtList(); $$ = slist; }
+    ;
+
+statement: assign T_SEMICOLON { $$ = $1; }
+
+assign: T_ID T_ASSIGN expr
+    { $$ = new AssignVarAST(*$1, $3); delete $1; }
+    ;
+
+rvalue: T_ID
+    { $$ = new VariableExprAST(*$1); delete $1; }
+    ;
+
+expr: rvalue
+    { $$ = $1; }
+    | constant
+    { $$ = $1; }
+    | expr T_PLUS expr
+    { $$ = new BinaryExprAST(T_PLUS, $1, $3); }
+    | expr T_MINUS expr
+    { $$ = new BinaryExprAST(T_MINUS, $1, $3); }
+    | expr T_MULT expr
+    { $$ = new BinaryExprAST(T_MULT, $1, $3); }
+    | expr T_DIV expr
+    { $$ = new BinaryExprAST(T_DIV, $1, $3); }
+    | expr T_MOD expr
+    { $$ = new BinaryExprAST(T_MOD, $1, $3); }
+    | T_MINUS expr %prec UMINUS 
+    { $$ = new UnaryExprAST(T_MINUS, $2); }
+    | T_NOT expr
+    { $$ = new UnaryExprAST(T_NOT, $2); }
+    | T_LPAREN expr T_RPAREN
+    { $$ = $2; }
+    ;
+
+constant: T_INTCONSTANT
+    { $$ = new NumberExprAST($1); }
+    | T_CHARCONSTANT
+    { $$ = new NumberExprAST($1); }
+    | bool_constant
+    { $$ = $1; }
+    ;
+
+bool_constant: T_TRUE
+    { $$ = new BoolExprAST(true); }
+    | T_FALSE 
+    { $$ = new BoolExprAST(false); }
+    ;
 
 %%
 
-
-%%
-
-
+int main() {
+  // parse the input and create the abstract syntax tree
+  int retval = yyparse();
+  return(retval >= 1 ? 1 : 0);
+}
